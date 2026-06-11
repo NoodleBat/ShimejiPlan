dc73ef1185b132137f3fd349e465ff6ae5cd71e2
NoodleBat <144660996+NoodleBat@users.noreply.github.com>
2026-06-11 11:35:11 +0300

    Create TaskItem.cs

---
+++ ShimejiPlan124325/Models/TaskItem.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShimejiPlan124325.Models
+{
+    public class TaskItem : INotifyPropertyChanged
+    {
+        private string _text;
+        private bool _isCompleted;
+
+        public string Text
+        {
+            get => _text;
+            set { _text = value; OnPropertyChanged(); }
+        }
+
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set { _isCompleted = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
