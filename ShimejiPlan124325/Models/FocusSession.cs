e9583a174fcb1bc9de12b883042323b15fed221f
NoodleBat <144660996+NoodleBat@users.noreply.github.com>
2026-06-11 13:15:01 +0300

    Add files via upload

---
+++ ShimejiPlan124325/Models/FocusSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShimejiPlan124325.Models
+{
+    public class FocusSession
+    {
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool IsWork { get; set; }
+        public string? TaskTitle { get; set; }
+    }
+}
